d8764c7001a4951e07cc7ea1e798c5db9a748f99
Daniel Smith <djssmith@hotmail.co.nz>
2021-01-15 10:06:07 +1300

    plesk fix

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./src/App";
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(process.env.PORT || 8080);
